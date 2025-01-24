140eba5e62be49e8e519257b3e0052b7e9ba6576
toxazol <antonmelni20@gmail.com>
2025-01-25 04:56:31 +0900

    simple bubble controller with a bird

--- Assets/Scripts/SoftBody.cs
+++ Assets/Scripts/SoftBody.cs
@@ -1,13 +1,25 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.U2D;
 
 public class SoftBody : MonoBehaviour
 {
-    public Transform[] points;
+    // public Transform[] points;
     public SpriteShapeController skin;
+
+    public float wobbleStrength = 0.1f;
+
+
+    List<Transform> points;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        points = new List<Transform>();
+        foreach (Transform obj in transform) {
+            if (obj.tag == "BubblePoint") {
+                points.Add(obj);
+            }
+        }
         UpdatePoints();
     }
 
@@ -15,11 +27,13 @@ public class SoftBody : MonoBehaviour
     void Update()
     {
         UpdatePoints();
+        Wobble();
     }
 
     void UpdatePoints()
     {
-        for (int i = 0; i < points.Length; i++) {
+        for (int i = 0; i < points.Count; i++) 
+        {
             
             skin.spline.SetPosition(i, points[i].localPosition);
 
@@ -28,6 +42,15 @@ public class SoftBody : MonoBehaviour
 
             skin.spline.SetLeftTangent(i, tangent.normalized * skin.spline.GetLeftTangent(i).magnitude);
             skin.spline.SetRightTangent(i, -tangent.normalized * skin.spline.GetRightTangent(i).magnitude);
+        }
+    }
+
+    void Wobble() 
+    {
+        for (int i = 0; i < points.Count; i++) 
+        {
+            Vector2 randomVec = Random.insideUnitCircle;
+            points[i].gameObject.GetComponent<Rigidbody2D>().AddForce(randomVec * wobbleStrength);
         }
     }
 }
