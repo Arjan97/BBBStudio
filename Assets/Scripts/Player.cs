7f98eb2f2b3836026921cfe82e1d9dd8b3c152ef
MSI\JohnKang <s901052002@yahoo.com.tw>
2025-01-25 03:24:20 +0900

    Add player move.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,8 +2,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    void Update()
+    InputSystem_Actions input;
+    Rigidbody2D rb;
+
+    private void Awake()
     {
-        
+        input = new InputSystem_Actions();
+        input.Player.Enable();
+        rb = transform.GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(new Vector2(0f, 1f));
+    }
+
+    void Update() {
+        if (input.Player.Dive.WasPressedThisFrame())
+            rb.AddForce(new Vector2(0f, -100f));
     }
 }
