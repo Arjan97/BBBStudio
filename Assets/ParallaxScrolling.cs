f70a2729d49a429d6f1518200140081522618ddc
Arjan97 <arjano97@gmail.com>
2025-01-25 08:57:17 +0100

    parallax fix

--- Assets/ParallaxScrolling.cs
+++ Assets/ParallaxScrolling.cs
@@ -2,38 +2,24 @@ using UnityEngine;
 
 public class ParallaxScrolling : MonoBehaviour
 {
-    public GameObject backgroundPrefab;
     public float scrollSpeed = 0.5f;
+    public Transform image1;
+    public Transform image2;
     public float resetDistance = 10f;
-
-    private GameObject currentBackground;
-    private GameObject nextBackground;
-    private float backgroundWidth;
-
-    private void Start()
-    {
-        currentBackground = gameObject;
-        backgroundWidth = resetDistance;
-
-        nextBackground = Instantiate(backgroundPrefab, GetNextPosition(), Quaternion.identity);
-        nextBackground.transform.SetParent(transform.parent);
-    }
 
     private void Update()
     {
-        transform.position += Vector3.left * scrollSpeed * Time.deltaTime;
+        image1.position += Vector3.left * scrollSpeed * Time.deltaTime;
+        image2.position += Vector3.left * scrollSpeed * Time.deltaTime;
 
-        if (transform.position.x <= -backgroundWidth)
+        if (image1.position.x <= -resetDistance)
         {
-            Destroy(currentBackground);
-            currentBackground = nextBackground;
-            nextBackground = Instantiate(backgroundPrefab, GetNextPosition(), Quaternion.identity);
-            nextBackground.transform.SetParent(transform.parent);
+            image1.position = new Vector3(image2.position.x + resetDistance, image1.position.y, image1.position.z);
         }
-    }
 
-    private Vector3 GetNextPosition()
-    {
-        return new Vector3(currentBackground.transform.position.x + backgroundWidth, currentBackground.transform.position.y, currentBackground.transform.position.z);
+        if (image2.position.x <= -resetDistance)
+        {
+            image2.position = new Vector3(image1.position.x + resetDistance, image2.position.y, image2.position.z);
+        }
     }
 }
