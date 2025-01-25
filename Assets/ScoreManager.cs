63cf608581b71f01197a5f97985e76af2c7f597e
Arjan97 <arjano97@gmail.com>
2025-01-25 14:45:58 +0100

    score manager instance add, cheatmanager prefab

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,62 @@
+using TMPro;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager Instance { get; private set; }
+
+    [SerializeField]
+    private int currentScore;
+
+    public float scoreIncreaseRate = 50f;
+
+    public delegate void ScoreUpdatedHandler(int newScore);
+    public event ScoreUpdatedHandler OnScoreUpdated;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        AddScore(Mathf.CeilToInt(scoreIncreaseRate * Time.deltaTime));
+    }
+
+    public int GetScore()
+    {
+        return currentScore;
+    }
+
+    public void AddScore(int amount)
+    {
+        currentScore += amount;
+        Debug.Log("Score Updated: " + currentScore);
+
+        OnScoreUpdated?.Invoke(currentScore);
+    }
+
+    public void ResetScore()
+    {
+        currentScore = 0;
+        Debug.Log("Score Reset.");
+
+        OnScoreUpdated?.Invoke(currentScore);
+    }
+
+    public void UpdateScoreDisplay(TMP_Text scoreText, float lerpSpeed)
+    {
+        if (scoreText != null)
+        {
+            float displayedScore = Mathf.Lerp(float.Parse(scoreText.text), currentScore, lerpSpeed * Time.deltaTime);
+            scoreText.text = Mathf.CeilToInt(displayedScore).ToString();
+        }
+    }
+}
